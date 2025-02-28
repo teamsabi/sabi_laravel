6dd9dd10b219b975c5f11c89814d14beb90c8028
teamsabi <sabiteam23@gmail.com>
2025-02-28 18:02:14 +0700

    Update Migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama_lengkap');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('no_whatsapp')->unique();
+            $table->string('foto_profil')->nullable();
+            $table->enum('role', ['admin', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_02_27_121656_create_kategori_donasi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kategori_donasi', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul_donasi');
+            $table->string('gambar');
+            $table->text('deskripsi');
+            $table->decimal('target_dana', 15, 2);
+            $table->integer('jumlah_donatur')->default(0);
+            $table->decimal('donasi_terkumpul', 15, 2)->default(0);
+            $table->date('dedline');
+            $table->timestamp('tanggal_buat')->useCurrent();
+            $table->enum('status', ['aktif', 'nonaktif'])->default('aktif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kategori_donasi');
+    }
+};
